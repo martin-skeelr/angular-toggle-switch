62919f2cd88e0e1b8aaf0afc5b1248464b8c3c4d
martinlaz <marin@skeelr.com>
2014-05-29 16:32:06 +0300

    feat(toggle-switch): add usage of ng-model
    add usage of ng-model instead of "model" attribute

--- angular-toggle-switch.js
+++ angular-toggle-switch.js
@@ -3,25 +3,29 @@ angular.module('toggle-switch', ['ng']).directive('toggleSwitch', function () {
     restrict: 'EA',
     replace: true,
     scope: {
-      model: '=',
+      // model: '=',
       disabled: '@',
       onLabel: '@',
       offLabel: '@',
       knobLabel: '@'
     },
-    template: '<div class="switch" ng-click="toggle()" ng-class="{ \'disabled\': disabled }"><div class="switch-animate" ng-class="{\'switch-off\': !model, \'switch-on\': model}"><span class="switch-left" ng-bind="onLabel"></span><span class="knob" ng-bind="knobLabel"></span><span class="switch-right" ng-bind="offLabel"></span></div></div>',
-    controller: function($scope) {
-      $scope.toggle = function toggle() {
-        if(!$scope.disabled) {
-          $scope.model = !$scope.model;
-        }
-      };
-    },
-    compile: function(element, attrs) {
+    require: 'ngModel',
+    template: '<div class="switch" ng-click="toggle()" ng-class="{ \'disabled\': disabled }"><div class="switch-animate" ng-class="{\'switch-off\': model, \'switch-on\': !model}"><span class="switch-left" ng-bind="onLabel"></span><span class="knob" ng-bind="knobLabel"></span><span class="switch-right" ng-bind="offLabel"></span></div></div>',
+    link:function(scope, element, attrs, NgModelController){
       if (!attrs.onLabel) { attrs.onLabel = 'On'; }
       if (!attrs.offLabel) { attrs.offLabel = 'Off'; }
       if (!attrs.knobLabel) { attrs.knobLabel = '\u00a0'; }
       if (!attrs.disabled) { attrs.disabled = false; }
+
+      NgModelController.$render = function() {
+        scope.model = NgModelController.$modelValue;
+      };
+      scope.toggle = function() {
+        if(!scope.disabled) {
+          NgModelController.$setViewValue(!NgModelController.$modelValue);
+          NgModelController.$render();
+        }
+      };
     },
   };
 });
